c9e3ccf808b1509299c49776c966034b4cdd7f6b
AlexVYard <alexvegayard@gmail.com>
2023-09-06 20:28:14 -0500

    all pokemon names displaying

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,35 +1,33 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AppService } from '../../app/app.services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule ],
 })
 
 export class HomePage {
 
   pokemonDB: any = [];
 
-  constructor(private service: AppService) {
+  constructor(private http: AppService) {
     console.log("HomePage Component")
-    /* this.http.get('https://pokeapi.co/api/v2/pokemon?limit=1010&offset=0', {})
-      .subscribe((data: any) => {
-        console.log(data);
-      }, (error: any) => {
-        console.error('Error:', error);
-      });   */
   }
 
   ngOnInit() {
     // get info from api
-    this.service.GetPokemonData().subscribe((data: any) => {
+    this.http.GetPokemonData().subscribe((data: any) => {
       
-      const pokedexFull: any = document.getElementById("pokedex");
-      let pokedexDataHTML = ''
+      this.pokemonDB = data.results
+
+      console.log(this.pokemonDB)
+
+      /* const pokedexFull: any = document.getElementById("cards");
 
       for (let i = 0; i < data.results.length; i++) {
 
@@ -37,25 +35,21 @@ export class HomePage {
         
         console.log(data.results[i])
 
-        pokedexDataHTML += `
-            <li class = "{this.pokemonDB[i].type[0]}" id= poketype>
-              <img class = "card-image" src ="{this.pokemonDB[i].img}"/>
-              <h2 class = "card-title">
-                {this.pokemonDB[i].num}
-                <br>
-                ${this.pokemonDB[i].name}
-              </h2>
-              <p class = "card-subtitle">
-                <div class="{this.pokemonDB[i].type[0]}Text">{this.pokemonDB[i].type[0]}</div>
-                <br>
-                <br>
-                <div class="{this.pokemonDB[i].type[1]}Text">{this.pokemonDB[i].type[1]}</div>
-              </p>
-            </li>
-          `;
-      }
+        const card = document.createElement('ion-card')
 
-      pokedexFull.innerHTML = pokedexDataHTML
+        const cardHeader = document.createElement('ion-card-header')
+
+        const cardTitle = document.createElement('ion-card-title')
+        cardTitle.className = "card-title"
+        cardTitle.innerHTML = this.pokemonDB[i].name
+
+        pokedexFull.appendChild(card)
+        card.appendChild(cardHeader)
+        cardHeader.appendChild(cardTitle)
+
+      } */
+
+      // pokedexFull.innerHTML = pokedexDataHTML
 
       // this.pokemonDB = data
 
